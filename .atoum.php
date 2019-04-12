983986f799d9384dcaf68947a91d22dc1cc51c89
Xavier Leune <xleune@ccmbenchmark.com>
2019-04-12 12:20:31 +0200

    Use html coverage report

---
+++ .atoum.php
@@ -0,0 +1,11 @@
+<?php
+    use mageekguy\atoum\reports;
+    use mageekguy\atoum\reports\coverage;
+    use mageekguy\atoum\writers\std;
+
+    $script->addDefaultReport();
+
+    $coverage = new coverage\html();
+    $coverage->addWriter(new std\out());
+    $coverage->setOutPutDirectory(__DIR__ . '/coverage');
+    $runner->addReport($coverage);
